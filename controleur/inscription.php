3780c81e55acb2ded93c69d6d24a9c8fd75b82fa
SachaFarinel <farinel.sacha@tutanota.com>
2022-01-10 16:23:11 +0100

    Add Bootstrap

---
+++ controleur/inscription.php
@@ -0,0 +1,48 @@
+<?php
+
+if ($_SERVER["SCRIPT_FILENAME"] == __FILE__) {
+    $racine = "..";
+}
+require "$racine/modele/bd.utilisateur.inc.php";
+
+// creation du menu burger
+$menuBurger = array();
+$menuBurger[] = array("url" => "./?action=connexion", "label" => "Connexion");
+$menuBurger[] = array("url" => "./?action=inscription", "label" => "Inscription");
+
+
+$inscrit = false;
+$msg = "";
+// recuperation des donnees GET, POST, et SESSION
+if (isset($_POST["mailU"]) && isset($_POST["mdpU"]) && isset($_POST["pseudoU"])) {
+
+    if ($_POST["mailU"] != "" && $_POST["mdpU"] != "" && $_POST["pseudoU"] != "") {
+        $mailU = $_POST["mailU"];
+        $mdpU = $_POST["mdpU"];
+        $pseudoU = $_POST["pseudoU"];
+
+        // enregistrement des donnees
+        $ret = addUtilisateur($mailU, $mdpU, $pseudoU);
+        if ($ret) {
+            $inscrit = true;
+        } else {
+            $msg = "l'utilisateur n'a pas été enregistré.";
+        }
+    } else {
+        $msg = "Renseigner tous les champs...";
+    }
+}
+
+if ($inscrit) {
+    // appel du script de vue qui permet de gerer l'affichage des donnees
+    $titre = "Inscription confirmée";
+    include "$racine/vue/entete.html.php";
+    include "$racine/vue/vueConfirmationInscription.php";
+    include "$racine/vue/pied.html.php";
+} else {
+    // appel du script de vue qui permet de gerer l'affichage des donnees
+    $titre = "Inscription pb";
+    include "$racine/vue/entete.html.php";
+    include "$racine/vue/vueInscription.php";
+    include "$racine/vue/pied.html.php";
+}
