381c037201828cda91a5054bb7d89f192c24f019
Bastien <bastien.lesueur56@gmail.com>
2022-01-20 22:33:19 +0100

    Fonction connexion ( à lire )
    J'vous partage mon avancée il reste des erreurs je corrige ça demain soir

--- libraries/classes/models/Patient.php
+++ libraries/classes/models/Patient.php
@@ -9,3 +9,53 @@ class Patient extends Model
 {
     protected $table = 'patient';
 }
+function login($mailU, $mdpU) {
+    if (!isset($_SESSION)) {
+        session_start();
+    }
+
+    //Bastien : Reprise du code du TP MVC d'ULRICH Fonction login
+    $util = getUtilisateurByMailU($mailU);
+    $mdpBD = $util["mdpU"];
+
+    if (trim($mdpBD) == trim(crypt($mdpU, $mdpBD))) {
+        // le mot de passe est celui de l'utilisateur dans la base de donnees
+        $_SESSION["mailU"] = $mailU;
+        $_SESSION["mdpU"] = $mdpBD;
+    }
+}
+
+function logout() {
+    if (!isset($_SESSION)) {
+        session_start();
+    }
+    unset($_SESSION["mailU"]);
+    unset($_SESSION["mdpU"]);
+}
+
+function getMailULoggedOn(){
+    if (isLoggedOn()){
+        $ret = $_SESSION["mailU"];
+    }
+    else {
+        $ret = "";
+    }
+    return $ret;
+        
+}
+
+function isLoggedOn() {
+    if (!isset($_SESSION)) {
+        session_start();
+    }
+    $ret = false;
+
+    if (isset($_SESSION["mailU"])) {
+        $util = getUtilisateurByMailU($_SESSION["mailU"]);
+        if ($util["mailU"] == $_SESSION["mailU"] && $util["mdpU"] == $_SESSION["mdpU"]
+        ) {
+            $ret = true;
+        }
+    }
+    return $ret;
+}
