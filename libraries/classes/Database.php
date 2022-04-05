6a4d5383dc7b7a593d141c1434b2034154121c9e
SachaFARINEL <farinel.sacha@tutanota.com>
2022-04-05 09:39:03 +0200

    Bonne version DATABASE

--- libraries/classes/Database.php
+++ libraries/classes/Database.php
@@ -36,11 +36,7 @@ class Database
         try {
             if (self::$instance === null) {
                 self::$instance = new PDO("mysql:host=$serveur;port=$port;dbname=$bd", $login, $mdp, $options);
-<<<<<<< Updated upstream
                 // self::$instance = new PDO("mysql:host=$serveurHome;dbname=$bdHome", $loginHome, $mdpHome, $options);
-=======
-                //self::$instance = new PDO("mysql:host=$serveurHome;dbname=$bdHome", $loginHome, $mdpHome, $options);
->>>>>>> Stashed changes
             }
             return self::$instance;
         } catch (PDOException $e) {
