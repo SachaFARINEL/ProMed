0d769bbcf27467fb5c2d10e0259c567c77717624
Bastien <bastien.lesueur56@gmail.com>
2022-05-04 23:57:13 +0200

    Jour + Heure en Français

--- templates/articles/espacePatient.html.php
+++ templates/articles/espacePatient.html.php
@@ -109,7 +109,11 @@ extract($donneesRdv);
                                                 <h5 class="card-title"><?= $id_praticien ?></h5>
                                                 <p class="card-text"><?= $id_prestation ?></p>
                                                 <p class="card-text"><?=
-                                                                        date_format(new DateTime($date), 'l jS F Y g:ia')
+
+                                                                        setlocale(LC_TIME, 'fr_FR.UTF-8', 'fr');
+                                                                        echo strftime("%A %d %B %G %H : %M", strtotime($date))
+                                                                        //echo strftime("%A %d %B %G", strtotime($date))
+                                                                        //date_format(new DateTime($date), 'l jS F Y g:ia')
                                                                         ?>
 
                                                 </p>
