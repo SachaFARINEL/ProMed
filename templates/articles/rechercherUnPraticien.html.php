1119ff8b9834c4f24aca0b7d51b8901a6992efc6
Sacha FARINEL <farinel.sacha@tutanota.com>
2022-05-05 10:54:51 +0200

    searchbox align

--- templates/articles/rechercherUnPraticien.html.php
+++ templates/articles/rechercherUnPraticien.html.php
@@ -1,8 +1,10 @@
-<div class="input-group rounded" style="width: 200px">
-    <input type="search" id="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
-    <span class="input-group-text border-0" id="search-addon">
-        <i class="bi bi-search"></i>
-    </span>
+<div class="container d-flex justify-content-center  my-5">
+    <div class="input-group rounded" style="width: 30vw">
+        <input type="search" id="search" class="form-control rounded" placeholder="Rechercher un praticien" aria-label="Search" aria-describedby="search-addon" />
+        <span class="input-group-text border-0" id="search-addon">
+            <i class="bi bi-search"></i>
+        </span>
+    </div>
 </div>
 
 <div id="content" style="text-align: center"></div>
