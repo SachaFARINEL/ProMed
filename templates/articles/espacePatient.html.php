0e5010ea28e8b844c1d79d1513095feec8bd2ce2
Sacha FARINEL <farinel.sacha@tutanota.com>
2022-05-21 16:13:46 +0200

    correction nom parties

--- templates/articles/espacePatient.html.php
+++ templates/articles/espacePatient.html.php
@@ -1,6 +1,6 @@
 <div class="container col-6">
     <div class="rdv rounded-3 bg-white shadow mt-4" style="font-family: Lato, sans-serif;height: 80vh; overflow-y: auto; overflow-x: hidden; display: flex; flex-direction: column; align-items: center">
-        <h2 class="mesRdv fs-4" style="text-align: center">Vos prochains rendez-vous</h2>
+        <h2 class="mesRdv fs-4" style="text-align: center">Mes prochains rendez-vous</h2>
         <?php
         // var_dump($allDataRDV);
         // exit;
