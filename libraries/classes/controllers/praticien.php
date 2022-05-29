6073442cf678238b269ad063081ad7167313646d
François LE PLAIN <77250304+Raydx@users.noreply.github.com>
2022-05-29 15:44:50 +0200

    fix adresse praticien

--- libraries/classes/controllers/praticien.php
+++ libraries/classes/controllers/praticien.php
@@ -99,6 +99,30 @@ class Praticien extends Controller
             'num_adelie',
             'nom_cabinet'
         ));
+        //Adresse
+        $numero = filter_input(INPUT_POST, 'numero', FILTER_SANITIZE_SPECIAL_CHARS);
+        $type_de_voie = filter_input(INPUT_POST, 'type_de_voie', FILTER_SANITIZE_SPECIAL_CHARS);
+        $adresse = filter_input(INPUT_POST, 'adresse', FILTER_SANITIZE_SPECIAL_CHARS);
+        $code_postal = filter_input(INPUT_POST, 'code_postal', FILTER_SANITIZE_NUMBER_INT);
+        $ville = filter_input(INPUT_POST, 'ville', FILTER_SANITIZE_SPECIAL_CHARS);
+        $departement = filter_input(INPUT_POST, 'departement', FILTER_SANITIZE_SPECIAL_CHARS);
+        $pays = filter_input(INPUT_POST, 'pays', FILTER_SANITIZE_SPECIAL_CHARS);
+        $role = filter_input(INPUT_GET, 'controller', FILTER_SANITIZE_SPECIAL_CHARS);
+
+        extract($this->model->findLastInsertId()); //Récupérationde l'id praticien pour le mettre dans la base adresse
+        $adresseModel = new \Models\Adresse();
+        $adresseModel->insert(compact(
+            'id_user',
+            'role',
+            'numero',
+            'type_de_voie',
+            'adresse',
+            'code_postal',
+            'ville',
+            'departement',
+            'pays'
+
+        ));
 
         \Http::redirect('?controller=praticien&task=showAuth');
     }
