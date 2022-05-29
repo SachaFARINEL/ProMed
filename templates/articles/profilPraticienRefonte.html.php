e4384d20a9f485892783686a730a0d91f9127d00
Sacha FARINEL <farinel.sacha@tutanota.com>
2022-05-29 16:30:07 +0200

    add cabinet to praticien profil

--- templates/articles/profilPraticienRefonte.html.php
+++ templates/articles/profilPraticienRefonte.html.php
@@ -11,7 +11,7 @@ extract($adressePraticien[0]);
 ?>
 <div class="col-12 mt-5" style="display: flex; justify-content: center">
     <div class="col-lg-3">
-        <div class="card mb-3" style='box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px; margin-right: 0.5rem; height: 35rem'>
+        <div class="card mb-3" style='box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px; margin-right: 0.5rem; height: 37rem'>
             <div class="card-body text-center">
                 <div>
                     <img src="./assets/images/doctor.svg" alt="avatar" class="rounded-circle img-fluid" style="width: 150px">
@@ -25,7 +25,7 @@ extract($adressePraticien[0]);
                 <p class="mt-3 text-muted mb-1">Numéro Adélie : <?= $num_adelie ?></p>
                 <hr>
                 <h5 class="mt-3">Adresse du cabinet</h5>
-
+                <p class="mt-3 text-muted mb-1"><?= $nom_cabinet ?></p>
                 <p class="mt-3 text-muted mb-1"><?= $numero . ' ' . $type_de_voie . ' ' . $adresse  ?></p>
                 <p class="mt-3 text-muted mb-1"><?= $code_postal . ', ' . $ville ?></p>
 
@@ -33,7 +33,7 @@ extract($adressePraticien[0]);
         </div>
     </div>
     <div class="col-lg-3">
-        <div class="card mb-3" style='box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px ; margin-left: 0.5rem; height: 35rem '>
+        <div class="card mb-3" style='box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px ; margin-left: 0.5rem; height: 37rem '>
             <div class="card-body font-monospace" style="overflow-y: auto">
                 <h4 class="mt-3" style="display: flex; justify-content: space-between">Mes prestations <img src="./assets/images/add.png" alt="avatar" class="parametresFiche rounded-circle img-fluid" id="addPresta" style="width: 8%"></h4>
                 <hr>
